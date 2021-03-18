cb44e49d66ade43bf8cfe5aea740a78089f8e6b4
hameji <hameji@gmail.com>
2021-03-18 22:49:30 +0900

    fileManagerを編集

--- fileManager.py
+++ fileManager.py
@@ -1,15 +1,23 @@
 import csv
 
-def write_csv_file(path, list):
-    with open(path, mode='w', encoding="utf-8_sig") as file:
+def write_to_csv(path, list):
+    with open(path, mode='w', encoding="utf-8_sig", newline="") as file:
         writer = csv.writer(file)
-        writer.writerow(list)
+        writer.writerows(list)
 
-def read_csv_file(path):
-    with open(path, mode='r', newline='', encoding="utf-8_sig") as file:
+def read_csv(path):
+    with open(path, mode='r', encoding="utf-8_sig", newline="") as file:
         reader = csv.reader(file)
         list = [row for row in reader]
         return list
+
+def add_to_csv(path, array):
+    print("input:", array)
+    list_data = read_csv(path)
+    print("before:", list_data)
+    list_data.append(array)
+    print("added:", list_data)
+    write_to_csv(path, list_data)
 
 # main処理
 def main():
