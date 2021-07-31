00aa7fb4f9d43a7966f14eb803a2ac868890d30f
takashi002013@gmail.com <takashi002013@gmail.com>
2021-07-31 19:39:36 +0900

    1

--- build.bat
+++
@@ -1,1 +0,0 @@
-python -m eel view.py html --onefile
--- gdrive_test.py
+++
@@ -1,5 +0,0 @@
-﻿from gdrive import *
-
-def test_download_file():
-    g = Gdrive("1qi-iyX9iOnV1XAHVtXFK0KUHvwqhp8GO")
-    g.download_file("img","item-64")
